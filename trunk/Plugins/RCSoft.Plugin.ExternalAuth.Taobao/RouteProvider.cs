09f3c150aa00c01ee575a45c0b3cc847d70eed51
jsczjone.lee@gmail.com <jsczjone.lee@gmail.com@188e67d7-1b1f-dcd9-c69c-2b97b1020884>
2013-07-10 15:08:20 +0000

    plugin

---
+++ trunk/Plugins/RCSoft.Plugin.ExternalAuth.Taobao/RouteProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using RCSoft.Web.Framework.Mvc.Routes;
+using System.Web.Routing;
+
+namespace RCSoft.Plugin.ExternalAuth.Taobao
+{
+    public partial class RouteProvider:IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute("Plugin.ExternalAuth.Taobao.PublicInfo",
+                 "Plugins/ExternalAuthTaobao/PublicInfo",
+                 new { controller = "ExternalAuthTaobao", action = "PublicInfo" },
+                 new[] { "RCSoft.Plugin.ExternalAuth.Taobao.Controllers" }
+            );
+
+            routes.MapRoute("Plugin.ExternalAuth.Taobao.Login",
+                 "Plugins/ExternalAuthTaobao/Login",
+                 new { controller = "ExternalAuthTaobao", action = "Login" },
+                 new[] { "RCSoft.Plugin.ExternalAuth.Taobao.Controllers" }
+                 );
+        }
+
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
